35667fe19d984c596b9d7c22a2cec8406e3e5296
Felix Benicourt <33630000+felixBenicourt@users.noreply.github.com>
2024-12-17 10:23:40 -0500

    Update userSetup.py

--- 1.0.0/hal_server/userSetup.py
+++ 1.0.0/hal_server/userSetup.py
@@ -1,3 +1,5 @@
+
+
 import socket
 import threading
 import json
@@ -6,17 +8,25 @@ import os
 
 BACKLOG = 5
 
-def execute_python_code(script_content, variables=None):
+print("userSetup.py from C:/Users/felix/OneDrive/Documents/maya/2020/scripts loaded !")
+
+def execute_python_script_from_path(script_path, variables=None):
     try:
         if variables is None:
             variables = {}
+
+        if not os.path.exists(script_path):
+            raise FileNotFoundError("Script file not found")
+
+        with open(script_path, 'r') as script_file:
+            script_content = script_file.read()
 
         exec_globals = globals().copy()
         exec_globals.update({'variables': variables})
 
         def maya_exec():
             try:
-                print("Executing code in Maya:")
+                print("Executing code in Maya from script:", script_path)
                 print(script_content)
                 exec(script_content, exec_globals)
             except Exception as e:
@@ -25,16 +35,6 @@ def execute_python_code(script_content, variables=None):
         maya.utils.executeDeferred(maya_exec)
     except Exception as e:
         print("Error setting up Python code execution:", e)
-
-def receive_complete_data(conn):
-    buffer_size = 4096
-    data = b""
-    while True:
-        part = conn.recv(buffer_size)
-        data += part
-        if len(part) < buffer_size:
-            break
-    return data.decode()
 
 def start_server(port):
     HOST = '127.0.0.1'
@@ -45,25 +45,30 @@ def start_server(port):
     while True:
         conn, addr = s.accept()
         print("Connected by:", addr)
-        try:
-            received_data = receive_complete_data(conn)
+        while True:
+            data = conn.recv(4096)
+            if not data:
+                break
+            received_data = data.decode()
             print("Received data:", received_data)
 
             data_dict = json.loads(received_data)
-            script_content = data_dict['script']
+            print("Loaded json data:", data_dict)
+            script_path = data_dict['script']
             variables = data_dict.get('variables', {})
 
-            execute_python_code(script_content, variables)
-        except Exception as e:
-            print("Error processing connection:", e)
-        finally:
-            conn.close()
+            execute_python_script_from_path(script_path, variables)
+
+        conn.close()
 
 def get_maya_instance_id():
     maya_pid = os.getpid()
     return maya_pid
 
+
 maya_instance_id = get_maya_instance_id()
 port = maya_instance_id
 server_thread = threading.Thread(target=start_server, args=(port,))
 server_thread.start()
+
+
